88773d3d77bb831fbfc3bd5bf441634e670734e3
Jaume Sanchez <the.spite@gmail.com>
2026-02-08 00:19:56 +0000

    Update graph.js

--- 8/graph.js
+++ 8/graph.js
@@ -4,9 +4,15 @@ import {
   LineBasicMaterial,
   Line as LineMesh,
   BufferGeometry,
+  Color,
   ArrowHelper,
   Group,
 } from "three";
+
+let minDistance = 0.5;
+let minAngle = Math.PI / 2 - 1;
+let maxAngle = Math.PI / 2 + 1;
+let probability = 0.95;
 
 const lines = [];
 
@@ -14,6 +20,72 @@ const nodes = [];
 const edges = [];
 
 const dt = 0.01;
+
+class SpatialGrid {
+  constructor(cellSize = 1) {
+    this.cellSize = cellSize;
+    this.cells = {};
+  }
+
+  _cellKey(ix, iz) {
+    return `${ix},${iz}`;
+  }
+
+  clear() {
+    this.cells = {};
+  }
+
+  insert(line) {
+    const x1 = Math.min(line.start.x, line.end.x);
+    const x2 = Math.max(line.start.x, line.end.x);
+    const z1 = Math.min(line.start.z, line.end.z);
+    const z2 = Math.max(line.start.z, line.end.z);
+
+    const ix0 = Math.floor(x1 / this.cellSize);
+    const ix1 = Math.floor(x2 / this.cellSize);
+    const iz0 = Math.floor(z1 / this.cellSize);
+    const iz1 = Math.floor(z2 / this.cellSize);
+
+    for (let ix = ix0; ix <= ix1; ix++) {
+      for (let iz = iz0; iz <= iz1; iz++) {
+        const key = this._cellKey(ix, iz);
+        if (!this.cells[key]) this.cells[key] = [];
+        this.cells[key].push(line.id);
+      }
+    }
+  }
+
+  build(lines) {
+    this.clear();
+    for (const line of lines) {
+      this.insert(line);
+    }
+  }
+
+  querySegment(start, end) {
+    const x1 = Math.min(start.x, end.x);
+    const x2 = Math.max(start.x, end.x);
+    const z1 = Math.min(start.z, end.z);
+    const z2 = Math.max(start.z, end.z);
+
+    const ix0 = Math.floor(x1 / this.cellSize);
+    const ix1 = Math.floor(x2 / this.cellSize);
+    const iz0 = Math.floor(z1 / this.cellSize);
+    const iz1 = Math.floor(z2 / this.cellSize);
+
+    const set = Object.create(null);
+    for (let ix = ix0; ix <= ix1; ix++) {
+      for (let iz = iz0; iz <= iz1; iz++) {
+        const key = this._cellKey(ix, iz);
+        const arr = this.cells[key];
+        if (!arr) continue;
+        for (const id of arr) set[id] = true;
+      }
+    }
+
+    return Object.keys(set).map((s) => Number(s));
+  }
+}
 
 function getNode(p) {
   const id = nodes.length;
@@ -84,17 +156,22 @@ class Line {
     this.direction.normalize().multiplyScalar(dt);
     this.active = true;
     this.parent = parent;
-    this.color = (Math.random() * 0xffffff) | 0;
+    this.color = new Color();
+    this.color.setHSL(
+      Math.random(),
+      Maf.randomInRange(0.5, 1),
+      Maf.randomInRange(0.25, 0.75),
+    );
   }
 
-  grow() {
+  grow(grid = null) {
     if (!this.active) {
       return;
     }
 
     this.end.add(this.direction);
 
-    const intersects = this.intersects();
+    const intersects = this.intersects(grid);
     if (intersects.length) {
       const end = getNode(intersects[0].point.clone());
       this.close(end);
@@ -111,7 +188,7 @@ class Line {
     }
 
     const l = this.end.distanceTo(this.start);
-    if (l > 1 && Math.random() > 0.995) {
+    if (l > minDistance && Math.random() > probability) {
       this.split();
     }
 
@@ -121,10 +198,21 @@ class Line {
     }
   }
 
-  intersects() {
+  intersects(grid = null) {
     const res = [];
-    for (const line of lines) {
-      if (line.id !== this.id) {
+
+    let candidateIds = null;
+    if (grid) {
+      candidateIds = grid.querySegment(this.start, this.end);
+    }
+
+    if (candidateIds && candidateIds.length) {
+      const seen = Object.create(null);
+      for (const id of candidateIds) {
+        if (id === this.id) continue;
+        if (seen[id]) continue;
+        seen[id] = true;
+        const line = lines[id];
         const i = getSegmentIntersection(
           this.start,
           this.end,
@@ -132,11 +220,25 @@ class Line {
           line.end,
           false,
         );
-        if (i) {
-          res.push({ id: line.id, point: i });
+        if (i) res.push({ id: line.id, point: i });
+      }
+    } else {
+      for (const line of lines) {
+        if (line.id !== this.id) {
+          const i = getSegmentIntersection(
+            this.start,
+            this.end,
+            line.start,
+            line.end,
+            false,
+          );
+          if (i) {
+            res.push({ id: line.id, point: i });
+          }
         }
       }
     }
+
     res.sort(
       (a, b) =>
         this.start.distanceToSquared(a.point) -
@@ -168,16 +270,18 @@ class Line {
     this.restart(newStart);
 
     const s = Math.random() > 0.5 ? 1 : -1;
-    const dir = this.direction
-      .clone()
-      .applyAxisAngle(up, (s * Math.PI) / 2 + Maf.randomInRange(-0.1, 0.1));
+    const angle = Maf.randomInRange(minAngle, maxAngle);
+    const dir = this.direction.clone().applyAxisAngle(up, angle * s);
     const line = new Line(this.startNode, dir, old.id);
     lines.push(line);
 
-    // if (Math.random() > 0.5) {
-    //   const line = new Line(this.startNode, dir.multiplyScalar(-1), old.id);
-    //   lines.push(line);
-    // }
+    if (Math.random() > 0.5) {
+      const dir2 = this.direction
+        .clone()
+        .applyAxisAngle(up, angle * s + Math.PI);
+      const line = new Line(this.startNode, dir2, old.id);
+      lines.push(line);
+    }
   }
 
   getPoints() {
@@ -202,10 +306,22 @@ function start(x, y, numLines = 1) {
 
 function update(n = 1) {
   for (let i = 0; i < n; i++) {
+    const grid = new SpatialGrid(2);
+    grid.build(lines);
+
     for (const line of lines) {
-      line.grow();
+      line.grow(grid);
     }
   }
+}
+
+function reset(options) {
+  minDistance = options.minDistance;
+  minAngle = options.minAngle;
+  maxAngle = options.maxAngle;
+  probability = options.probability;
+
+  lines.length = 0;
 }
 
 const material = new LineBasicMaterial({ color: 0x000000 });
@@ -238,4 +354,4 @@ function draw() {
   return res;
 }
 
-export { start, update, draw };
+export { start, update, draw, reset };
