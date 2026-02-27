e9828d3e4add6c731ba59da10cd0947d41b78bcf
Jaume Sanchez <the.spite@gmail.com>
2026-02-27 22:56:09 +0000

    Update particles.js

--- 9/particles.js
+++ 9/particles.js
@@ -7,10 +7,13 @@ import {
   Object3D,
   Mesh,
   IcosahedronGeometry,
-  MeshBasicMaterial,
-  DoubleSide,
-  AdditiveBlending,
+  Color,
+  HalfFloatType,
+  RepeatWrapping,
 } from "three";
+import { Material } from "modules/material.js";
+import { ShaderPass } from "modules/shader-pass.js";
+import { shader as orthoVS } from "shaders/ortho-vs.js";
 
 const fragmentShader = `
 precision highp float;
@@ -75,7 +78,7 @@ void main() {
   float lon = agentPos.x * 6.28318530;
   float lat = (agentPos.y - 0.5) * 3.14159265;
   vec3 worldPos = life > 0.0
-    ? vec3(cos(lat) * cos(lon), sin(lat), cos(lat) * sin(lon)) 
+    ? vec3(cos(lat) * cos(lon), sin(lat), cos(lat) * sin(lon))
     : vec3(1000.0);
 
   vec4 mvPos = modelViewMatrix * vec4(worldPos, 1.0);
@@ -90,75 +93,109 @@ void main() {
   vec2 ndcOffset = position.xy * pointSize * parabola(life) * vTrail * 2.0 / resolution;
   vDisc = position.xy;
   vUv = uv;
-  vLife = life; 
+  vLife = life;
   gl_Position = vec4(clip.xy + ndcOffset * clip.w, clip.z, clip.w);
+}
+`;
+
+const blurFS = `
+precision highp float;
+
+uniform sampler2D tInput;
+uniform int blurRadius;
+uniform vec2 blurDir;
+
+in vec2 vUv;
+out vec4 fragColor;
+
+void main() {
+  vec2 texSize = vec2(textureSize(tInput, 0));
+  float sigma = float(blurRadius) * 0.5;
+  vec4 color = vec4(0.0);
+  float total = 0.0;
+  for (int i = -blurRadius; i <= blurRadius; i++) {
+    float fi = float(i);
+    float w = exp(-fi * fi / (2.0 * sigma * sigma));
+    color += texture(tInput, vUv + fi * blurDir / texSize) * w;
+    total += w;
+  }
+  fragColor = color / total;
 }
 `;
 
 const vertexShaderSphere = `
 precision highp float;
 
-in vec3 position;
-in vec2 uv; 
-
-uniform sampler2D tTrail;
+uniform mat4 modelMatrix;
 uniform mat4 modelViewMatrix;
-uniform mat4 projectionMatrix;  
+uniform mat4 projectionMatrix;
+uniform mat3 normalMatrix;
+uniform sampler2D tTrailBlurred;
+uniform float displacementOffset;
 
+in vec3 position;
+in vec3 normal;
+in vec2 uv;
+
+out vec3 vPosition;
+out vec3 vViewPosition;
+out vec3 vWorldPosition;
+out vec3 vNormal;
 out vec2 vUv;
-out float vDepth;
-
-vec4 blur(in sampler2D image, in vec2 TexCoords, in float radius, in vec2 resolution) {
-  vec4 color = vec4(0.0);
-  float total = 0.0;
-  
-  for (float x = -radius; x <= radius; x++) {
-      for (float y = -radius; y <= radius; y++) {
-          
-          // Calculate weight (Gaussian function)
-          // Sigma is usually radius / 2.0
-          float sigma = radius / 2.0;
-          float weight = exp(-(x*x + y*y) / (2.0 * sigma * sigma));
-          
-          vec2 offset = vec2(x, y) / resolution;
-          color += texture(image, TexCoords + offset) * weight;
-          total += weight;
-      }
-  }
-  color /= total;
-  return color;
-}
 
 void main() {
   vUv = uv;
+  vPosition = position;
 
-  vec4 t = blur(tTrail, uv, 20.0, vec2(textureSize(tTrail, 0)));
-  float trail = clamp(t.r / 200.0, 0.0, 1.0);
+  vec2 texRes = vec2(textureSize(tTrailBlurred, 0));
+  float trail = clamp(texture(tTrailBlurred, uv).r / 200.0, 0.0, 1.0);
 
-  vec3 p = position * (1. + trail / 10.);
+  vec3 p = position * (1.0 + trail * displacementOffset / 10.0);
+
+  // Compute displaced normal from trail gradient
+  float epsU = 4.0 / texRes.x;
+  float epsV = 4.0 / texRes.y;
+  float trailR = clamp(texture(tTrailBlurred, uv + vec2(epsU, 0.0)).r / 200.0, 0.0, 1.0);
+  float trailU = clamp(texture(tTrailBlurred, uv + vec2(0.0, epsV)).r / 200.0, 0.0, 1.0);
+  float dTrail_du = (trailR - trail) / epsU;
+  float dTrail_dv = (trailU - trail) / epsV;
+
+  float lon = uv.x * 6.28318530;
+  float lat = (uv.y - 0.5) * 3.14159265;
+  vec3 T = normalize(vec3(-sin(lon), 0.0, cos(lon)));
+  vec3 B = normalize(vec3(-sin(lat)*cos(lon), cos(lat), -sin(lat)*sin(lon)));
+
+  vec3 displacedNormal = normalize(normal - displacementOffset * (dTrail_du * T + dTrail_dv * B) / 10.0);
+
+  vec4 worldPos = modelMatrix * vec4(p, 1.0);
+  vWorldPosition = worldPos.xyz;
   vec4 mvPos = modelViewMatrix * vec4(p, 1.0);
-
-  vec3 localPos = mat3(modelViewMatrix) * p;
-  vDepth = (localPos.z + 1.) * .5;
-
+  vViewPosition = -mvPos.xyz;
+  vNormal = normalize(normalMatrix * displacedNormal);
   gl_Position = projectionMatrix * mvPos;
 }
 `;
 
-const fragmentShaderSphere = `
-precision highp float;
-
-in vec2 vUv;
-in float vDepth;
-
-uniform sampler2D tTrail; 
-
-out vec4 fragColor;
+const sphereFragMain = `
+uniform sampler2D tTrailNormal;
+uniform float displacementOffset;
 
 void main() {
-  float trail = clamp(texture(tTrail, vUv).r / 200.0, 0.0, 1.0);
-  vec3 color = vec3(trail) * (.1 + .9 * vDepth);
-  fragColor = vec4(color,1.);
+  mat3 viewMatrixInverse = mat3(inverse(viewMatrix));
+  vec3 worldNormal = normalize(viewMatrixInverse * vNormal);
+
+  vec2 ts = 1.0 / vec2(textureSize(tTrailNormal, 0));
+  float hR = texture(tTrailNormal, vUv + vec2(ts.x, 0.0)).r;
+  float hL = texture(tTrailNormal, vUv - vec2(ts.x, 0.0)).r;
+  float hU = texture(tTrailNormal, vUv + vec2(0.0, ts.y)).r;
+  float hD = texture(tTrailNormal, vUv - vec2(0.0, ts.y)).r;
+  vec3 mapN = normalize(vec3((hL - hR) * displacementOffset, (hD - hU) * displacementOffset, 50.0));
+  worldNormal = perturbNormal2Arb(vWorldPosition, worldNormal, vUv, mapN);
+
+  vec4 diffuseColor = vec4(color, 1.0);
+  vec3 outgoingLight = shade(vWorldPosition, worldNormal, vUv, diffuseColor, roughness, metalness);
+  outgoingLight = ACESFilmicToneMapping(outgoingLight);
+  fragColor = linearToSRGB(vec4(outgoingLight, 1.0));
 }
 `;
 
@@ -188,22 +225,61 @@ class SceneParticles {
     );
     this.mesh.frustumCulled = false;
 
-    this.sphere = new Mesh(
-      new IcosahedronGeometry(1, 40),
-      new RawShaderMaterial({
-        vertexShader: vertexShaderSphere,
-        fragmentShader: fragmentShaderSphere,
+    const blurFBOOptions = {
+      type: HalfFloatType,
+      wrapS: RepeatWrapping,
+      wrapT: RepeatWrapping,
+      depthBuffer: false,
+    };
+
+    const makeBlurPass = (blurDir, radius) => {
+      const mat = new RawShaderMaterial({
         uniforms: {
-          tTrail: { value: null },
+          tInput: { value: null },
+          blurRadius: { value: radius },
+          blurDir: { value: blurDir },
         },
+        vertexShader: orthoVS,
+        fragmentShader: blurFS,
         glslVersion: GLSL3,
-        side: DoubleSide,
-        depthTest: false,
-        depthWrite: false,
-        transparent: true,
-        blending: AdditiveBlending,
-      }),
-    );
+      });
+      const pass = new ShaderPass(mat, blurFBOOptions);
+      pass.setSize(2048, 1024);
+      return { mat, pass };
+    };
+
+    const bH = makeBlurPass(new Vector2(1, 0), 20);
+    const bV = makeBlurPass(new Vector2(0, 1), 20);
+    this.blurHMat = bH.mat;
+    this.blurHPass = bH.pass;
+    this.blurVMat = bV.mat;
+    this.blurVPass = bV.pass;
+    this.blurVMat.uniforms.tInput.value = this.blurHPass.texture;
+
+    const nH = makeBlurPass(new Vector2(1, 0), 5);
+    const nV = makeBlurPass(new Vector2(0, 1), 5);
+    this.normalBlurHMat = nH.mat;
+    this.normalBlurHPass = nH.pass;
+    this.normalBlurVMat = nV.mat;
+    this.normalBlurVPass = nV.pass;
+    this.normalBlurVMat.uniforms.tInput.value = this.normalBlurHPass.texture;
+
+    this.sphereMat = new Material({
+      vertexShader: vertexShaderSphere,
+      uniforms: {
+        color: new Color(1, 1, 1),
+        roughness: 0.2,
+        metalness: 0.5,
+      },
+      customUniforms: {
+        tTrailBlurred: { value: this.blurVPass.texture },
+        tTrailNormal: { value: this.normalBlurVPass.texture },
+        displacementOffset: { value: 0.0 },
+      },
+      main: sphereFragMain,
+    });
+
+    this.sphere = new Mesh(new IcosahedronGeometry(1, 40), this.sphereMat);
     this.sphere.scale.z = -1;
 
     // this.group.add(this.mesh);
@@ -214,7 +290,20 @@ class SceneParticles {
     this.mat.uniforms.positions.value = simTexture;
     this.mat.uniforms.tTrail.value = trailTexture;
 
-    this.sphere.material.uniforms.tTrail.value = trailTexture;
+    this.blurHMat.uniforms.tInput.value = trailTexture;
+    this.normalBlurHMat.uniforms.tInput.value = trailTexture;
+  }
+
+  renderBlur(renderer) {
+    this.blurHPass.render(renderer);
+    this.blurVPass.render(renderer);
+    this.normalBlurHPass.render(renderer);
+    this.normalBlurVPass.render(renderer);
+  }
+
+  syncMaterial(renderer, scene) {
+    this.sphereMat.syncLights(scene);
+    this.sphereMat.syncRenderer(renderer);
   }
 
   setSize(renderer, width, height) {
